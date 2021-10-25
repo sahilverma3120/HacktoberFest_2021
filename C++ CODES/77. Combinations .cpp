e90e77a73ce122748f0a3b22590b0249d9d0cf12
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-10-25 22:54:43 +0530

    Create 77. Combinations .cpp

---
+++ C++ CODES/77. Combinations .cpp
@@ -0,0 +1,23 @@
+Combinations
+Medium
+Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+Example 2:
+
+Input: n =

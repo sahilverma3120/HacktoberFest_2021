1ee655538de730a04d3058cd10b1e1859620193a
samayraina <72222867+samayraina@users.noreply.github.com>
2021-10-26 23:18:15 +0530

    Added The Code

--- java Codes/Invert Binary Tree.java
+++ java Codes/Invert Binary Tree.java
@@ -13,3 +13,13 @@
  *     }
  * }
  */
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+}

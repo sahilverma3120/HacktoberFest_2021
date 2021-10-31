943a36e8e7d8b4c1ca13d6d8990a65640d1e3d16
HAJAREPRIYANKA <69968932+HAJAREPRIYANKA@users.noreply.github.com>
2021-10-31 16:21:06 +0530

    Update 5. Longest Palindromic Substring.cpp

--- C++ CODES/5. Longest Palindromic Substring.cpp
+++ C++ CODES/5. Longest Palindromic Substring.cpp
@@ -1,9 +1,19 @@
 Given a string s, return the longest palindromic substring in s.
-
+string longestPalindrome(string s) {
+        if(s.size()<2)return s;
+        int len=s.size();
+        for(int m=0;m<len;){
+            if(len-m<mxl/2) break;
+            int l=m-1,r=m+1;
+            while(r<len&&s[r]==s[m])r++;
+            m=r;//next diff middle
+            while(l>=0&&r<len && s[l]==s[r]){l--,r++;};
+            if(r-l-1>mxl){
+                mxl=r-l-1,from=l+1;
+            }
+        }
+        return s.substr(from,mxl);
+    }
  
 
-Example 1:
 
-Input: s = "babad"
-Output: "bab"
-Note: "aba" is also a valid answer.
